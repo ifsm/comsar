e708d1d7bb54709706b6e8aa0492a061acda705f
Michael Blaß <michael.blass@uni-hamburg.de>
2020-05-15 19:06:02 +0200

    Fixed imports

--- comsar/__init__.py
+++ comsar/__init__.py
@@ -1,6 +1,6 @@
 import pkg_resources as _pkg
 
-from comsar.tracks.timbre import TimbreTrack
-from comsar.tracks.utilities import TrackResult
+from .tracks.timbre import TimbreTrack
+from .tracks.utilities import TrackResult
 
 __version__ = _pkg.get_distribution('comsar').version
