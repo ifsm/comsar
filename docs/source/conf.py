b0461db706c81ae94437b19fadb8053d5e9ddabf
Michael Blaß <mblass@posteo.net>
2021-04-23 07:22:54 +0200

    Set the correct version and release numnbers.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,8 +25,8 @@ copyright = '2021, Michael Blaß'
 author = 'Michael Blaß '
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.3'
-version = '0.0.3'    # needed for epub
+release = '0.0.2'
+version = '0.0.2'    # needed for epub
 
 
 # -- General configuration ---------------------------------------------------
